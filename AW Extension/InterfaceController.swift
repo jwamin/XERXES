27260e4320cfa2df89e3a9eca171d7e5dd949495
Joss Manger <Jwam3rd@gmail.com>
2018-03-18 19:12:57 -0400

    Starts AW version

---
+++ AW Extension/InterfaceController.swift
@@ -0,0 +1,45 @@
+//
+//  InterfaceController.swift
+//  AW Extension
+//
+//  Created by Joss Manger on 3/18/18.
+//  Copyright © 2018 Joss Manger. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    var player: WKAudioFilePlayer!
+    
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+   
+        if let fileURL = Bundle.main.url(forResource: "xxrmsg01", withExtension: "wav"){
+            let asset = WKAudioFileAsset(url: fileURL)
+            let playerItem = WKAudioFilePlayerItem(asset: asset)
+            print(fileURL)
+            player = WKAudioFilePlayer(playerItem: playerItem)
+            player.play()
+        } else {
+            print("something didnt world")
+        }
+
+        
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
--- XERXES/ViewController.swift
+++ XERXES/ViewController.swift
@@ -7,13 +7,13 @@
 //
 
 import UIKit
-import AVFoundation
 
 class ViewController: UIViewController {
-    var audioPlayer:AVAudioPlayer!
+    
+    var player:XEPlayer!
     override func viewDidLoad() {
         super.viewDidLoad()
-        audioPlayer = AVAudioPlayer()
+        player = XEPlayer()
         // Do any additional setup after loading the view, typically from a nib.
     }
     
@@ -23,114 +23,10 @@ class ViewController: UIViewController {
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        do{
-            var sounds:[String] = ViewController.audioFiles
-            
-            if(UserDefaults.standard.bool(forKey: "include_routines")){
-                sounds.append(contentsOf: ViewController.dull_files)
-            }
-            
-            let index = Int(arc4random_uniform(UInt32(sounds.count)))
-            print("picked \(index) of \(sounds.count): \(sounds[index])")
-            if let asset = NSDataAsset(name:sounds[index]){
-                audioPlayer = try AVAudioPlayer(data: asset.data)
-                audioPlayer.play()
-            } else {
-                print("some kind of error")
-            }
-            
-        }
-            
-        catch{
-            print(error)
-        }
+       player.play()
     }
     
     
-    static let audioFiles = [
-        "XXpdie01",
-        "XXpdie02",
-        "XXpdie03",
-        "XXpdie04",
-        "XXpdie05",
-        "XXpdie06",
-        "XXpdie07",
-        "XXpdie08",
-        "xx4and5c",
-        "xx4xor5c",
-        "xxalrtov",
-        "xxdorhak",
-        "xxhakexp",
-        "xxhaksec",
-        "xxral101",
-        "xxran001",
-        "xxrephak",
-        "xxrmsg01",
-        "xxrmsg02",
-        "xxrmsg03",
-        "xxrmsg04",
-        "xxrmsg05",
-        "xxrmsg06",
-        "xxrmsg07",
-        "xxrmsg08",
-        "xxrmsg09",
-        "xxrmsg10",
-        "xxrmsg11",
-        "xxrmsg12",
-        "xxyal001"
-    ];
-    
-    static let dull_files = [
-        "xxral201",
-        "xxral202",
-        "xxral203",
-        "xxral204",
-        "xxral205",
-        "xxral206",
-        "xxral207",
-        "xxral208",
-        "xxral209",
-        "xxral210",
-        "xxral211",
-        "xxral212",
-        "xxral213",
-        "xxral214",
-        "xxral215",
-        "xxral216",
-        "xxral217",
-        "xxral218",
-        "xxral219",
-        "xxral220",
-        "xxral221",
-        "xxral222",
-        "xxral223",
-        "xxral224",
-        "xxral225",
-        "xxral226",
-        "xxral227",
-        "xxtur001",
-        "xxtur002",
-        "xxtur003",
-        "xxtur004",
-        "xxtur005",
-        "xxtur006",
-        "xxtur007",
-        "xxtur008",
-        "xxtur009",
-        "xxtur010",
-        "xxtur011",
-        "xxtur012",
-        "xxtur013",
-        "xxtur014",
-        "xxtur015",
-        "xxtur016",
-        "xxtur017",
-        "xxtur018",
-        "xxtur019",
-        "xxtur020",
-        "xxtur021",
-        "xxtur022"
-    ];
-    
+  
 }
 
